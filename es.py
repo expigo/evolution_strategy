5f4654cb660cdc4c090dd583515f7023a2bd57be
Kryspin Musiol <kryspin.musiol@gmail.com>
2021-06-12 12:59:17 +0200

    well, trying to optimize

--- es.py
+++ es.py
@@ -13,20 +13,19 @@ def es(obj, orig_data, mu_size=200, ratio=5, Nmax=200, eps=10e-5):
 
     parents_x = np.array([np.random.default_rng().uniform(low=-10, high=10, size=(N_PROBLEM_PARAMS,)) for _ in range(mu_size)])
     sigmas = np.array([np.random.default_rng().uniform(low=0, high=10, size=(N_ALG_PARAMS,)) for _ in range(mu_size)])
-    mu = np.hstack((parents_x, sigmas))
-
     zero_mean_gauss = lambda x: np.random.normal(0, x)
-    simgas_mutated = np.zeros(shape=(mu_size, 3))
-
 
     epochs = []
     for epoch in range(Nmax):
+        fitnesses = np.apply_along_axis(rmse, 1, parents_x).reshape(-1, 1)
+        mu = np.hstack((parents_x, sigmas, fitnesses))
+
         mutation_operand = np.vectorize(zero_mean_gauss)(sigmas)
         offspring = parents_x + mutation_operand
 
         plus = np.vstack([parents_x, offspring])
 
-        plus_sorted = sorted(plus, key=lambda x: rmse(*x))
+        plus_sorted = sorted(plus, key=lambda x: rmse(x))
         parents_x = np.array(plus_sorted[:200])
 
 
@@ -42,7 +41,8 @@ def es(obj, orig_data, mu_size=200, ratio=5, Nmax=200, eps=10e-5):
     return parents_x, mu, mutation_operand, parents_x, offspring
 
 def calc_rmse(data):
-    def fn(a, b, c):
+    def fn(x):
+        [a, b, c] = x
         f = lambda i: a*(i ** 2 - b*np.cos(c*np.pi*i))
         x = np.arange(-5, 5, 0.1)
         y = f(data[:, 0])
@@ -62,14 +62,7 @@ if __name__ == "__main__":
     mu = res[3]
 
     rmse = calc_rmse(data)
-    results = []
-    for i in range(len(mu)):
-        # print(*mu[i, :])
-        results.append(rmse(*mu[i, :]))
-
-    results = sorted(results)
-
-    mu_sorted = sorted(mu, key=lambda x: rmse(*x))
+    mu_sorted = sorted(mu, key=lambda x: rmse(x))
 
     results2 = []
     for i in range(len(mu_sorted)):
